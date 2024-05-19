384529b03e58dd6730125d5e7a973b0823681ddf
Esdras Marcelino da Silva Junior <esdras.marcelino@hotmail.com>
2024-05-19 16:24:39 -0300

    Add project files.

---
+++ G64.ProdutoApi/Models/Ingrediente.cs
@@ -0,0 +1,28 @@
+﻿namespace G64.ProdutoApi.Models;
+
+public class Ingrediente
+{
+    public Guid Id { get; private set; }
+    public string Descricao { get; private set; }
+
+    private Ingrediente(Guid id, string descricao)
+    {
+        Id = id;
+        Descricao = descricao;
+    }
+
+    public static Ingrediente CriaIngrediente(Guid id, string descricao)
+    {
+        ValidaDescricao(descricao);
+        return new Ingrediente(id, descricao);
+    }
+
+    private static void ValidaDescricao(string descricao)
+    {
+        if (string.IsNullOrEmpty(descricao))
+        {
+            throw new ArgumentException("Descricao deve estar preenchida");
+        }
+    }
+
+}
